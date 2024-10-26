d4e6164ab89f8c79d062c588e009629e8fb77307
thirumal-22 <167775995+thirumal-22@users.noreply.github.com>
2024-10-26 17:15:01 +0530

    Create 4c.py

---
+++ 4c.py
@@ -0,0 +1,6 @@
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    return '', 204
