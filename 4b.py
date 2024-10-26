0b44a3b9a810fd68ab38a3fab41fc6f68e192fc6
thirumal-22 <167775995+thirumal-22@users.noreply.github.com>
2024-10-26 17:14:27 +0530

    Create 4b.py

---
+++ 4b.py
@@ -0,0 +1,7 @@
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    data = request.get_json()
+    product.name = data.get('name', product.name)
+    db.session.commit()
+    return jsonify(product.serialize()), 200
